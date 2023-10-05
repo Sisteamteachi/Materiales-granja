47d2875ed3cd58fb2727e5d2358330e2c24e7ffc
Juan Pablo Jaramillo Giraldo <juanpablogiraldo9602@gmail.com>
2023-10-04 23:38:57 -0500

    se añaden nuevos features, hovers y animaciones

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-window.onload
+
+
+function bigImg(x) {
+  x.style.height = "64px";
+  x.style.width = "64px";
+}
+
+function normalImg(x) {
+  x.style.height = "32px";
+  x.style.width = "32px";
+}
