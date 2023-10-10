380d39522a4a1272668174820de3d9a23caab215
ibarguen-dev <jt158161@gmail.com>
2023-10-09 23:00:09 -0500

    se organizan las animaciones

--- js/animaciones.js
+++ js/animaciones.js
@@ -6,21 +6,23 @@ gsap.from(".banner-section", { duration: 2.5, fadein: true, x: -600});
 gsap.from(".img1", { duration: 2.5, fadein: true, x:-280});
 gsap.from(".img2", { duration: 2.5, fadein: true, x:-200});
 gsap.from(".img3", { duration: 2.5, fadein: true, x:-100});
-gsap.from(".container-section2", { duration: 3.5, fadein: false, y:80});
+//gsap.from(".container-section2", { duration: 3.5, fadein: false, y:80});
 
 
 
-gsap.to(".container-section2", {
+gsap.from(".container-section2__div", {
   opacity: 1,                  // Opacidad final (visible)
-    y: -600,                        // Posición final (opcional: 0 para eliminar el desplazamiento vertical)
+    y: 600,                        // Posición final (opcional: 0 para eliminar el desplazamiento vertical)
     duration: 1,                 // Duración de la animación (en segundos)
     scrollTrigger: {
       trigger: ".container-section2", // Elemento que dispara la animación
       start: "top 70%",         // Comienza la animación cuando el 80% del elemento es visible
       end: "bottom 60%",    // Termina la animación cuando el centro del elemento alcanza el centro de la ventana
       scrub: true,              // Animación suave durante el desplazamiento
-      toggleActions: "play none none reverse" // Comportamiento de la animación al desplazar
-      }
+      toggleActions: "play none none reverse", // Comportamiento de la animación al desplazar
+      //markers:true
+      },
+      color:"transparent",
   });
 
 
