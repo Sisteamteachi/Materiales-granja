f9ac00804268a0a44ed2024a47f63cf61e6fc63d
ibarguen-dev <jt158161@gmail.com>
2023-10-27 13:07:12 -0500

    se crea un menu, un footer dos contatntes de js y una pages

---
+++ Materiales_y_servicios_la_granja/src/App.jsx
@@ -0,0 +1,18 @@
+import HomePage from './Pages/HomePages'
+import Footer from './component/footer'
+import Navbar from './component/navbar'
+import './css/App.css'
+
+function App() {
+
+
+  return (
+    <main>
+      <Navbar/>
+      <HomePage/>
+      <Footer/>
+    </main>
+  )
+}
+
+export default App
