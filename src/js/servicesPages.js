23c2e671b8fff5878779f00a9bc992fc83ed17b4
ibarguen-dev <jt158161@gmail.com>
2024-08-15 18:52:47 -0500

    se crean archivos adccionales

---
+++ src/js/servicesPages.js
@@ -0,0 +1,65 @@
+export const Maquinaria = [
+    {
+        img:"/sany-horizontal.webp",
+        title:"Cargador  930",
+        text:" Cargador 930 Caterpillar capacidad de dos metros cúbicos"
+    },
+    {
+        img:"/Cargador-horizontal.webp",
+        title:"Volqueta doble troque",
+        text:"Capacidad es hasta 14 metros cúbicos"
+    },
+    {
+        img:"/Volque-doble-troque-horizontal-azul.webp",
+        title:"Volqueta doble troque",
+        text:"Capacidad  es hasta 14 metros cúbicos"
+    },
+    {
+        img:"/volqueta-doble-troque-blanca-horizontal.webp",
+        title:"Volqueta ",
+        text:"Capacidad  es hasta 7 metros cúbicos"
+    },
+    {
+        img:"/volqueta-blanca.webp",
+        title:"volqueta",
+        text:"Capacidad  es hasta 7 metros cúbicos"
+    },
+]
+
+export const Materiales = [
+    {
+        img:"/sany-horizontal.webp",
+        title:"Sub bases ",
+        text:" Materia procesada sub base"
+    },
+    {
+        img:"/Cargador-horizontal.webp",
+        title:"Arena de concreto",
+        text:"Materia procesada arena de concreto"
+    },
+    {
+        img:"/Volque-doble-troque-horizontal-azul.webp",
+        title:"Sub base",
+        text:"Materia prima procesada sub base"
+    },
+    {
+        img:"/volqueta-doble-troque-blanca-horizontal.webp",
+        title:"Gravilla ",
+        text:"Materia prima procesada gravilla"
+    },
+    {
+        img:"/volqueta-blanca.webp",
+        title:"Piedra filtro",
+        text:"Materia prima procesada"
+    },
+    {
+        img:"/volqueta-blanca.webp",
+        title:"Triturado",
+        text:"Materia prima procesada Triturado"
+    },
+    {
+        img:"/volqueta-blanca.webp",
+        title:"Piedra gavion",
+        text:"Materia prima no procesada"
+    },
+]
---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	
+	theme: {
+		extend: {
+			margin:{
+				'8%':'8%',
+				'6%':'6%'
+			},
+			fontSize:{
+				
+			}
+		},
+	},
+	important: true,
+	plugins: [],
+}
