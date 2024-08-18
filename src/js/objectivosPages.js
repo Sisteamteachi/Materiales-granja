639b02527ec36eaf309dd96a5f3fa5669e1a5bbf
ibarguen-dev <jt158161@gmail.com>
2024-08-17 19:40:10 -0500

    se suben los cambios

---
+++ src/js/objectivosPages.js
@@ -0,0 +1,24 @@
+export const Title = ""
+export const objectivos = [
+  {
+      img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/carrotanque.webp?alt=media&token=1d0d06f8-ddd6-4f77-ba97-9f3843243287",
+      title:"Carro tanque",
+      body:" Carro tanque utilizado principalmente para el transporte de alimentos. "
+  },
+  {
+      img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/carrotanque2.webp?alt=media&token=339f97fe-a264-417a-906c-3cb96ed6fc24",
+      title:"Carro tanque",
+      body:"Carro tanque utilizado principalmente para el transporte de alimentos."
+  },
+  {
+      img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/carrotanque3.webp?alt=media&token=9ce6540a-807b-44dc-a66c-c7359c6ba767",
+      title:"Carro tanque",
+      body:"Carro tanque utilizado principalmente para el transporte de alimentos."
+  },
+  {
+      img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/carrotanque4.webp?alt=media&token=080e06f3-7f30-4f27-a09a-be718a665f05",
+      title:"Carro tanque ",
+      body:"Carro tanque utilizado principalmente para el transporte de alimentos."
+  },
+
+]
