3755c3bd87810b5264f413f7c811ad9d92a472ef
ibarguen-dev <jt158161@gmail.com>
2024-08-18 23:00:24 -0500

    se sueben elementos del js

--- src/js/objectivosPages.js
+++ src/js/objectivosPages.js
@@ -1,24 +1,28 @@
-export const Title = ""
+export const Title = "Objectivos"
 export const objectivos = [
-  {
-      img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/carrotanque.webp?alt=media&token=1d0d06f8-ddd6-4f77-ba97-9f3843243287",
-      title:"Carro tanque",
-      body:" Carro tanque utilizado principalmente para el transporte de alimentos. "
-  },
-  {
-      img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/carrotanque2.webp?alt=media&token=339f97fe-a264-417a-906c-3cb96ed6fc24",
-      title:"Carro tanque",
-      body:"Carro tanque utilizado principalmente para el transporte de alimentos."
-  },
-  {
-      img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/carrotanque3.webp?alt=media&token=9ce6540a-807b-44dc-a66c-c7359c6ba767",
-      title:"Carro tanque",
-      body:"Carro tanque utilizado principalmente para el transporte de alimentos."
-  },
-  {
-      img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/carrotanque4.webp?alt=media&token=080e06f3-7f30-4f27-a09a-be718a665f05",
-      title:"Carro tanque ",
-      body:"Carro tanque utilizado principalmente para el transporte de alimentos."
-  },
-
+    {
+        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/sany-horizontal.webp?alt=media&token=fac5ad11-3c4e-4fbc-b792-db18a1e7159e",
+        title:"Innovación",
+        body:" Potenciar la presencia en línea para expandir la visibilidad, llegar a una audiencia diversa y aprovechar las oportunidades de negocio en la web "
+    },
+    {
+        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/Cargador-horizontal.webp?alt=media&token=173acc0f-76ef-4883-ba10-d44a1d8655fb",
+        title:"Sostenibilidad",
+        body:"Proyectar nuestro profesionalismo, confianza y credibilidad, influyendo positivamente en la toma de decisiones de los visitantes sobre hacer negocios con la empresa"
+    },
+    {
+        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/trituradora.webp?alt=media&token=0335bf27-9ac6-431d-8e9c-c4778ef055d1",
+        title:"Analisis de clientes",
+        body:"Recopilación de la información y estadísticas para respaldar la toma de decisiones."
+    },
+    {
+        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/Triturado.webp?alt=media&token=38e3eead-4b81-4bb8-b7e2-86f0acfea3aa",
+        title:"Visibilidad ",
+        body:"Ampliar el alcance en medios digitales para llegar a una audiencia más extensa de clientes"
+    },
+    {
+        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/Volque-doble-troque-horizontal-azul.webp?alt=media&token=1d988701-f95a-4a03-9fe6-38ad616b406d",
+        title:"Soluciones verdes",
+        body:"Fomentamos la preservación ambiental mediante prácticas eco-amigables, incluyendo la gestión responsable de materias no procesables o utilizables."
+    }
 ]
