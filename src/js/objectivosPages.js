3fe89f19d7400fad834f0d35efba19a19195fdff
ibarguen-dev <jt158161@gmail.com>
2026-08-22 23:03:04 -0500

    se realiza un actualización el diseño de la pagina en la seguridad

--- src/js/objectivosPages.js
+++ src/js/objectivosPages.js
@@ -1,27 +1,27 @@
 export const Title = "Objectivos"
 export const objectivos = [
     {
-        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/sany-horizontal.webp?alt=media&token=fac5ad11-3c4e-4fbc-b792-db18a1e7159e",
+        img:"./img/sany-horizontal.webp",
         title:"Innovación",
         body:" Potenciar la presencia en línea para expandir la visibilidad, llegar a una audiencia diversa y aprovechar las oportunidades de negocio en la web "
     },
     {
-        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/Cargador-horizontal.webp?alt=media&token=173acc0f-76ef-4883-ba10-d44a1d8655fb",
+        img:"./img/Cargador-horizontal.webp",
         title:"Sostenibilidad",
         body:"Proyectar nuestro profesionalismo, confianza y credibilidad, influyendo positivamente en la toma de decisiones de los visitantes sobre hacer negocios con la empresa"
     },
     {
-        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/trituradora.webp?alt=media&token=0335bf27-9ac6-431d-8e9c-c4778ef055d1",
+        img:"./img/trituradora.webp",
         title:"Analisis de clientes",
         body:"Recopilación de la información y estadísticas para respaldar la toma de decisiones."
     },
     {
-        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/Triturado.webp?alt=media&token=38e3eead-4b81-4bb8-b7e2-86f0acfea3aa",
+        img:"./img/Triturado.webp",
         title:"Visibilidad ",
         body:"Ampliar el alcance en medios digitales para llegar a una audiencia más extensa de clientes"
     },
     {
-        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/Volque-doble-troque-horizontal-azul.webp?alt=media&token=1d988701-f95a-4a03-9fe6-38ad616b406d",
+        img:"./img/Volque-doble-troque-horizontal-azul.webp",
         title:"Soluciones verdes",
         body:"Fomentamos la preservación ambiental mediante prácticas eco-amigables, incluyendo la gestión responsable de materias no procesables o utilizables."
     }
--- src/js/servicesPages.js
+++ src/js/servicesPages.js
@@ -6,34 +6,34 @@ export const titleMaquinaria = "Maquinaria"
 export const titleMateriales = "Materiales"
 
 export const BannerFooterPages = {
-    video:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/volqueta.mp4?alt=media&token=3bb18de7-635d-48d0-9d14-6f1e80364649",
+    video:"./video/volqueta.mp4",
     text:"Servicio de carga y descarga de materias primas y servicios de desbloqueos en derrumbes"
 }
 
 
 export const Maquinaria = [
     {
-        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/sany-horizontal.webp?alt=media&token=fac5ad11-3c4e-4fbc-b792-db18a1e7159e",
+        img:"./img/sany-horizontal.webp",
         title:"Cargador  930",
         text:" Cargador 930 Caterpillar capacidad de dos metros cúbicos"
     },
     {
-        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/Cargador-horizontal.webp?alt=media&token=173acc0f-76ef-4883-ba10-d44a1d8655fb",
+        img:"./img/Cargador-horizontal.webp",
         title:"Volqueta doble troque",
         text:"Capacidad es hasta 14 metros cúbicos"
     },
     {
-        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/Volque-doble-troque-horizontal-azul.webp?alt=media&token=1d988701-f95a-4a03-9fe6-38ad616b406d",
+        img:"./img/Volque-doble-troque-horizontal-azul.webp",
         title:"Volqueta doble troque",
         text:"Capacidad  es hasta 14 metros cúbicos"
     },
     {
-        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/volqueta-doble-troque-blanca-horizontal.webp?alt=media&token=01289ea8-26db-411e-899f-7221000ce105",
+        img:"./img/volqueta-doble-troque-blanca-horizontal.webp",
         title:"Volqueta ",
         text:"Capacidad  es hasta 7 metros cúbicos"
     },
     {
-        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/volqueta-blanca.webp?alt=media&token=1e50759c-9e10-4b25-bdb8-400fe4f42507",
+        img:"./img/volqueta-blanca.webp",
         title:"volqueta",
         text:"Capacidad  es hasta 7 metros cúbicos"
     },
@@ -41,37 +41,38 @@ export const Maquinaria = [
 
 export const Materiales = [
     {
-        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/Sub-bases1.webp?alt=media&token=c35dabdb-284e-4771-aecc-06eff7b63b59",
+        img:"./img/Sub-bases1.webp",
         title:"Sub bases ",
         text:" Materia procesada sub base"
     },
     {
-        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/Arena-de-concreto.webp?alt=media&token=5bd570fc-aa69-42c2-977c-d34bff6e5bed",
+        img:"./img/Arena-de-concreto.webp",
         title:"Arena de concreto",
         text:"Materia procesada arena de concreto"
     },
     {
-        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/Sub-base2.webp?alt=media&token=42995bba-fb9b-4cf5-a3cd-3f64d051a37a",
+        img:"./img/Sub-base2.webp",
         title:"Sub base",
         text:"Materia prima procesada sub base"
     },
     {
-        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/Gravilla.webp?alt=media&token=264ec548-06fd-4efa-b035-a7b978d3c159",
+        img:"./img/Gravilla.webp",
+        title:"Gravilla ",
         title:"Gravilla ",
         text:"Materia prima procesada gravilla"
     },
     {
-        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/Piedra-filtro.webp?alt=media&token=09c83709-ce92-45a1-924c-95f9bc06f8d1",
+        img:"./img/Piedra-filtro.webp",
         title:"Piedra filtro",
         text:"Materia prima procesada"
     },
     {
-        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/Triturado.webp?alt=media&token=3d08b165-2e27-4941-b01b-18311bce46d7",
+        img:"./img/Triturado.webp",
         title:"Triturado",
         text:"Materia prima procesada Triturado"
     },
     {
-        img:"https://firebasestorage.googleapis.com/v0/b/materialesgranja-d7e7d.appspot.com/o/Piedra-gavion.webp?alt=media&token=1a87ce81-1a8a-4c6a-a8be-aac9cda56edc",
+        img:"./img/Piedra-gavion.webp",
         title:"Piedra gavion",
         text:"Materia prima no procesada"
     },
